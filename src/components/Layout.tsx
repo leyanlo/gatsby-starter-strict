25a2201f31386f27693fda9edf821203cb446349
Leyan Lo <leyanlo@gmail.com>
2019-09-15 03:52:10 -0700

    feat(css): install sanitize.css

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,7 @@
+import 'sanitize.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/typography.css';
+
 import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import { Helmet } from 'react-helmet';
